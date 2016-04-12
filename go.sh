2afdcaaa1f5c5ce57abacb19c2f3fe731da762c1
Luoxy2016 <646951310@qq.com>
2016-04-12 16:53:10 +0800

    新增上传脚本

---
+++ go.sh
@@ -0,0 +1,7 @@
+git config --global user.email "646951310@qq.com"
+git config --global user.name "Luoxy2016"
+git config --global push.default simple
+git add .
+git commit -m $1
+git push 
+start http://Luoxy2016.github.io
